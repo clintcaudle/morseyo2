39a658319b2905755a4ee6b5ae7793145ad6e311
Ryan H Volum <ryanhvolum@dh-10-20-66-29.vuse.vanderbilt.edu>
2013-02-05 10:10:05 -0600

    Initial Commit

---
+++ Morse Code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Morse Code
+//
+//  Created by Ryan Volum on 2/5/13.
+//  Copyright (c) 2013 Vanderbilt University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
